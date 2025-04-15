f41d52e4fda3fd144a1fa0197a7bae4fcee53916
mizzcereal <sangwookpen96@gmail.com>
2025-04-15 17:33:24 +0900

    0320 report추가가

--- oop/0320/0320_report.cpp
+++ oop/0320/0320_report.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main(){
 
-    // 양의 정수 입력 ex 12345 % 10
-    // 자릿수 입력 : 2
-    // 출력 2번째 자릿수에 있는 정수는 4입니다.
+    long long n;
+    int a, result;
 
-    int a;
-    int n;
-
-    cout << "양의 정수 입력 : ";
-    cin >> a;
-    cout << "자릿 수 입력 : ";
+    cout << "정수 입력 : ";
     cin >> n;
-    cout << (a /static_cast<int>(pow(10,n -1))) % 10;
-    return 0;
+    cout << "자릿수 입력 : ";
+    cin >> a;
+
+    result = (int)(n / pow(10,a-1)) % 10;
+
+    if((int)(n / pow(10, a-1)) != 0){
+        cout << "오른쪽에서 시작해서 " << a << "번째 자리에 있는 숫자는 " << result;
+    }else{
+        cout << "자릿수 범위를 벗어났습니다.";    
+
+    }
+
+
 }
